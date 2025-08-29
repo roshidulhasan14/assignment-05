1a244772444ddb18b55eb25ee7636b323f5ef2c8
Roshidul Hasan <roshidulhasan14@gmail.com>
2025-08-29 21:47:26 +0600

    responsive section

--- script/index.js
+++ script/index.js
@@ -1,73 +1,67 @@
 
 
-
-
-
 <script>
- let likeCount = 0;
- let coinCount = 100;
- let copyCount = 0;
+    let likeCount = 0;
+    let coinCount = 100;
+    let copyCount = 0;
 
 
- document.querySelectorAll(".fa-heart").forEach(heart => {
-    heart.addEventListener("click", () => {
-        likeCount++;
-        document.getElementById("likeCount").innerText = likeCount;
-    });
+document.querySelectorAll(".fa-heart").forEach(heart => {
+  heart.addEventListener("click", () => {
+    likeCount++;
+    document.getElementById("likeCount").innerText = likeCount;
+  });
 });
 
 
- function copyNumber(number) {
-    navigator.clipboard.writeText(number).then(() => {
-        alert(`Number ${number} copied to clipboard `);
-        copyCount++;
-        document.querySelector(".navbar-right button").innerText = copyCount + " copy";
-    }).catch(err => {
-        console.error("Failed to copy: ", err);
-    });
-}
-
-
- function callNumber(serviceName, number) {
-    if (coinCount < 20) {
-        alert("Not enough coins to make a call ");
-        return;
+function copyNumber(number) {
+  navigator.clipboard.writeText(number).then(() => {
+    alert(`Number ${number} copied to clipboard `);
+    copyCount++;
+    const navbarBtn = document.querySelector(".navbar-right button");
+    if (navbarBtn) {
+      navbarBtn.innerText = copyCount + " copy";
     }
-
-    coinCount -= 20;
-    document.getElementById("coinCount").innerText = coinCount;
-
-    alert(`Calling ${serviceName} of ${number} `);
-
-    
-    const now = new Date();
-    const time = now.toLocaleTimeString();
-
-
-    const historyList = document.getElementById("callHistory");
-    const li = document.createElement("li");
-    li.idName = "fire";
-    li.innerHTML = `<span>${serviceName}, (${number})</span><span id="time">${time}</span>`;
-    historyList.prepend(li);
+  }).catch(err => {
+    console.error("Failed to copy: ", err);
+  });
 }
 
 
- document.addEventListener("DOMContentLoaded", function () {
-    const clearBtn = document.querySelector("#call-box button");
-    const historyList = document.getElementById("callHistory");
+function callNumber(serviceName, number) {
+  if (coinCount < 20) {
+    alert("Not enough coins to make a call ");
+    return;
+  }
 
+  coinCount -= 20;
+  document.getElementById("coinCount").innerText = coinCount;
+
+  alert(`Calling ${serviceName} at ${number} `);
+
+  
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+
+  
+  const historyList = document.getElementById("callHistory");
+  const li = document.createElement("li");
+  li.id = "fire"; 
+  li.className = "flex justify-between border-b pb-1";
+  li.innerHTML = `<span>${serviceName} (${number})</span><span class="text-gray-400">${time}</span>`;
+  historyList.prepend(li);
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const clearBtn = document.querySelector("#call-box button");
+  const historyList = document.getElementById("callHistory");
+
+  if (clearBtn) {
     clearBtn.addEventListener("click", function () {
-        historyList.innerHTML = "";
+      historyList.innerHTML = "";
     });
+  }
 });
-
 
 </script>
-
-
-
-
-
-
-
-
